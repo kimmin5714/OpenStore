f5724bb89aa4c172b582d020244a51cfa112ec52
user.email <kjm1994@naver.com>
2023-11-16 17:53:40 +0900

    찬영 rightMenu 작성 진행 중 231116-17:53

--- final_buk04_10_09_Vue/src/components/apart/MapMenu/MapMenu.vue
+++ final_buk04_10_09_Vue/src/components/apart/MapMenu/MapMenu.vue
@@ -1,10 +1,12 @@
 <script setup>
 import LeftMenuContainer from "@/components/apart/MapMenu/LeftMenu/LeftMenuContainer.vue";
+import RightMenuContainer from "@/components/apart/MapMenu/RightMenu/RightMenuContainer.vue";
 </script>
 
 <template>
   <div id="map-menu">
     <LeftMenuContainer />
+    <RightMenuContainer />
   </div>
 </template>
 
